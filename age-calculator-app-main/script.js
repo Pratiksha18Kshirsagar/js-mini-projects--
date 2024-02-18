12265d0193a37a869af33b2f213c614529b111de
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-02-18 10:00:32 +0530

    18-02-24

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,14 +1,15 @@
 let btn = document.querySelector("img");
-
+let input = document.querySelector("input");
+let day = document.querySelector(".Iday");
+let month = document.querySelector(".Imonth");
+let year = document.querySelector(".Iyear");
 
 
 btn.addEventListener("click", () => {
-    let day = document.querySelector(".Iday");
-    let month = document.querySelector(".Imonth");
-    let year = document.querySelector(".Iyear");
-    day.value = " ";
-    year.value = " ";
-    month.value = " ";
+   
+    // day.value = " ";
+    // year.value = " ";
+    // month.value = " ";
 
 
     var today = new Date();
@@ -30,8 +31,9 @@ btn.addEventListener("click", () => {
         days = tempDate.getDate() - birthDate.getDate() + today.getDate();
     }
 
-    
-    if (day.value == " " || month.value == " " || year.value == " ") {
+
+    if (day.value == "" || month.value == "" || year.value == "") {
+        console.log(day.value)
         document.querySelectorAll("p")[0].style.color = "red";
         document.querySelectorAll("p")[2].style.color = "red";
         document.querySelectorAll("p")[4].style.color = "red";
@@ -44,12 +46,35 @@ btn.addEventListener("click", () => {
         document.querySelectorAll("p")[1].innerText = "This field is required";
         document.querySelectorAll("p")[3].innerText = "This field is required";
         document.querySelectorAll("p")[5].innerText = "This field is required";
-        
-
-        
     }
+    else if(day.value > 12 && month.value > 31){
+        document.querySelectorAll("p")[0].style.color = "red";
+        document.querySelectorAll("p")[1].style.color = "red";
+        day.style.borderColor = "red";
+        document.querySelectorAll("p")[1].innerText = "This field is invalid";
+        document.querySelectorAll("p")[2].style.color = "red";
+        document.querySelectorAll("p")[3].style.color = "red";
+        month.style.borderColor = "red";
+        document.querySelectorAll("p")[3].innerText = "This field is invalid";
+
+    }
+    else if(day.value > 31 ){
+        document.querySelectorAll("p")[0].style.color = "red";
+        document.querySelectorAll("p")[1].style.color = "red";
+        day.style.borderColor = "red";
+        document.querySelectorAll("p")[1].innerText = "This field is invalid";
+    }
+    else if(month.value > 12 ){
+        document.querySelectorAll("p")[2].style.color = "red";
+        document.querySelectorAll("p")[3].style.color = "red";
+        month.style.borderColor = "red";
+        document.querySelectorAll("p")[3].innerText = "This field is invalid";
+    }
+   
+
 
     else {
+        console.log(day.value);
         let yr = document.querySelectorAll("span")[0];
         yr.innerText = years;
         let mth = document.querySelectorAll("span")[1];
@@ -59,7 +84,24 @@ btn.addEventListener("click", () => {
     }
 
 
+
 });
+
+input.addEventListener("input" , ()=>{
+    document.querySelectorAll("p")[0].style.color = "black";
+    document.querySelectorAll("p")[2].style.color = "black";
+    document.querySelectorAll("p")[4].style.color = "black";
+    document.querySelectorAll("p")[1].style.color = "black";
+    document.querySelectorAll("p")[3].style.color = "black";
+    document.querySelectorAll("p")[5].style.color = "black";
+    day.style.borderColor = "black";
+    year.style.borderColor = "black";
+    month.style.borderColor = "black";
+    document.querySelectorAll("p")[1].innerText = " ";
+    document.querySelectorAll("p")[3].innerText = " ";
+    document.querySelectorAll("p")[5].innerText = " ";
+
+})
 
 
 
